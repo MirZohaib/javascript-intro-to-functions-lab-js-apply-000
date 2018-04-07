d6235d8487c8628c53f7310d8cf4ae3f1378318d
mir.zohaib@hotmail.com <mir.zohaib@hotmail.com>
2018-04-07 20:23:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string)
+{
+  string.toLowerCase() ==="hello"
+  return "I can't hear you"
+  string.toUpperCase() === "HELLO"
+  return "YES INDEED!"
+  }
