e4b4ffa18014b154d247eeeae001d7cf0e4de336
mir.zohaib@hotmail.com <mir.zohaib@hotmail.com>
2018-04-07 20:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ function sayHiToGrandma(string)
 {
   if (string.toLowerCase() === string)
   {
-    return "I can't hear you!";
+    return "I can't hear you!"
   } 
-  else if (string.toUpperCase() === string)
+   if (string.toUpperCase() === string)
   {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") 
+    return "YES INDEED!"
+  } 
+   if (string === "I love you, Grandma.") 
   {
-    return "I love you, too.";
+    return "I love you, too."
  }
 }
